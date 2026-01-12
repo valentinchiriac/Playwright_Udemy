d93d73934229d1d40b80cb830531fa41e24d9cff
Valentin Chiriac <Valentin.Chiriac@endava.com>
2026-01-12 13:48:17 +0200

    mocking and API work

--- api-calls/getLoginToken.js
+++ api-calls/getLoginToken.js
@@ -1,9 +1,9 @@
 import * as nodeFetch from "node-fetch"
-export const getLoginToken = async () => {
+export const getLoginToken = async (username, password) => {
     //API injection with username and password for login
     const response = await nodeFetch("http://localhost:2221/api/login", {
         method: "POST",
-        body: JSON.stringify({"username":"admin","password":"Admin123"})
+        body: JSON.stringify({"username": username,"password": password})
     })
     if(response.status !== 200){
         throw new Error("An error occured trying to retrieve the login token.")
--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,17 +1,28 @@
 import { test } from "@playwright/test"
 import { MyAccountPage } from "./../page-objects/MyAccountPage.js"
 import { getLoginToken } from "./../api-calls/getLoginToken.js"
+import { adminDetails } from "./../data/userDetails.js"
 
-test.only("My account using cookie injection", async ({page}) => {
+test.only("My account using cookie injection and mocking network request", async ({page}) => {
     //make a request to get login token
-    const loginToken = await getLoginToken()
-    console.warn({loginToken})
+    const loginToken = await getLoginToken(adminDetails.username, adminDetails.password)
+    
+    page.route("**/api/user**", async (route, request) => {
+        await route.fulfill({
+            status: 500,
+            contentType:"application/json",
+            body: JSON.stringify({message: "PLAYWRIGHT ERROR FOR MOCKING"}),
+        })
+    })
+    
     //inject the login token into the browser
     const myAccount = new MyAccountPage(page)
     await myAccount.visit()
+    await page.pause()
     await page.evaluate(([loginTokenInsideBrowserCode]) => {
         document.cookie = "token=" + loginTokenInsideBrowserCode
     }, [loginToken])
     await myAccount.visit()
     await myAccount.waitForPageHeading()
+    await myAccount.waitForErrorMessage()
 })
